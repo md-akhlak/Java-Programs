0387c33f2b6e4b55b47ba49fb2dd1fcd487cbaf2
M Akhlak <md.22scse1010882@galgotiasuniversity.edu.in>
2023-11-01 21:46:38 +0530

    Added in Array.java

--- MyFirstJava/Array.java
+++ MyFirstJava/Array.java
@@ -1,8 +1,20 @@
 public class Array {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i);
+//        for (int i = 0; i < 10; i++) {
+//            System.out.println(i);
+//        }
 
-        }
+        int a = 10;
+        int b = 40;
+
+        int ans = sum(a, b);
+        System.out.println(ans);
+
     }
+
+    static int sum(int a, int b) {
+        return a + b;
+    }
+
+
 }
