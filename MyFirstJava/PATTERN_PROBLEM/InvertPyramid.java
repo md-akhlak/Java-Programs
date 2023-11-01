1b37dee04c2769e19a1407311c9b740c0890e455
M Akhlak <md.22scse1010882@galgotiasuniversity.edu.in>
2023-11-02 00:38:10 +0530

    learned Patter problem

--- MyFirstJava/PATTERN_PROBLEM/InvertPyramid.java
+++ MyFirstJava/PATTERN_PROBLEM/InvertPyramid.java
@@ -2,6 +2,19 @@ package PATTERN_PROBLEM;
 
 public class InvertPyramid {
     public static void main(String[] args) {
+        int n = 5;
+
+        for(int i = 1; i <= n; i++){
+            // for print space " "
+            for(int j = 1; j<= i-i; j++){
+                System.out.print(" ");
+            }
+            // for print *
+            for(int j = 1; j <= i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
         
     }
     
