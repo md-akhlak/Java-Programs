6f4187d275422b992262086fc78cd5135554882f
M Akhlak <md.22scse1010882@galgotiasuniversity.edu.in>
2023-11-16 00:11:11 +0530

    added

---
+++ MyFirstJava/JAVA_OOPS/CopyConstructor.java
@@ -0,0 +1,36 @@
+package JAVA_OOPS;
+
+public class CopyConstructor {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "akhlak";
+        s1.age = 19;
+        Student s2 = new Student(s1);
+        System.out.println(s1.name);
+
+    }
+}
+
+class Student {
+    String name;
+    int roll;
+    int age;
+    int marks;
+
+    Student() {
+    }
+
+    Student(String name, int roll) {
+        this.name = name;
+        this.roll = roll;
+
+    }
+
+    Student(Student s) {
+        this.name = s.name;
+        // this.roll = s.roll;
+        this.age = s.age;
+        // this.marks = s.marks;
+    }
+
+}
---
+++ MyFirstJava/JAVA_OOPS/Inheritance.java
@@ -0,0 +1,31 @@
+package JAVA_OOPS;
+
+public class Inheritance {
+    public static void main(String[] args) {
+        Lion l1 = new Lion();
+        l1.name = "girrafe";
+        // l1.color();
+        l1.animalName();
+        // l1.eat();
+
+    }
+}
+
+class Animal {
+
+    String name;
+
+    void animalName() {
+        System.out.println("name is name" + name);
+    }
+
+    void color() {
+        System.out.println("color is color");
+    }
+}
+
+class Lion extends Animal {
+    void eat() {
+        System.out.println("eats meat");
+    }
+}
