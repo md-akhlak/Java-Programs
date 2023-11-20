6c1590f1a865c60665fdc644f83856d285594abf
M Akhlak <md.22scse1010882@galgotiasuniversity.edu.in>
2023-11-20 23:38:04 +0530

    learned Abstraction in OOPs

--- MyFirstJava/JAVA_OOPS/ABSTRACTION/AbsractionInJava.java
+++ MyFirstJava/JAVA_OOPS/ABSTRACTION/AbsractionInJava.java
@@ -1,11 +1,5 @@
 package JAVA_OOPS.ABSTRACTION;
 
-public class AbsractionInJava {
-    public static void main(String[] args) {
-
-    }
-}
-
 /*
  * Absraction :- Hiding all the Unnecessary details and showing only the
  * important parts to the user
@@ -13,16 +7,58 @@ public class AbsractionInJava {
  * It can be implemented using two ways :- Abstract class and Interfaces
  */
 
-abstract class BaseClass {
-    void display() {
-        System.out.println("This is Base class");
-    }
+/*
+ * -> cannot create an instance of abstract class
+ * -> can have abstract/non-abstract methods
+ * -> can have constructor
+ */
 
-    // abstract void color() {
-    // }
+public class AbsractionInJava {
+    public static void main(String[] args) {
+        Horse h1 = new Horse();
+        h1.eat();
+        h1.walk();
+        h1.color = "red";
+        System.out.println(h1.color);
+
+        Chicken c1 = new Chicken();
+        c1.eat();
+        c1.walk();
+        c1.color = "pink";
+        System.out.println(c1.color);
+    }
 }
 
-class Derived extends BaseClass {
+abstract class Animal {
+    String color;
+
+    Animal() {
+        color = "brown";
+    }
+
+    void eat() {
+        System.out.println("animal eats");
+    }
+
+    abstract void walk();
+}
+
+class Horse extends Animal {
     void color() {
+        color = "White";
+    }
+
+    void walk() {
+        System.out.println("Horse have four legs");
+    }
+}
+
+class Chicken extends Animal {
+    void color() {
+        color = "yellow";
+    }
+
+    void walk() {
+        System.out.println("walk on 2 legs");
     }
 }
