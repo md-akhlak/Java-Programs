681d4cb4f65915cc1379bc46f27779c4425fe77b
M Akhlak <md.22scse1010882@galgotiasuniversity.edu.in>
2023-11-22 02:48:55 +0530

    added

--- MyFirstJava/Divide_and_Conquer/MergeSort.java
+++ MyFirstJava/Divide_and_Conquer/MergeSort.java
@@ -2,46 +2,45 @@ package Divide_and_Conquer;
 
 public class MergeSort {
 
-    // static void mergingSort(int arr[], int start, int end) {
-    // if (start >= end) {
-    // return;
-    // }
-    // int mid = start + (end - start) / 2;
+    static void mergingSort(int arr[], int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        int mid = start + (end - start) / 2;
 
-    // // work
-    // mergingSort(arr, start, mid);
-    // mergingSort(arr, mid + 1, end);
-    // merging(arr, start, mid, end);
-    // }
+        // work
+        mergingSort(arr, start, mid);
+        mergingSort(arr, mid + 1, end);
 
-    // public static void merging(int arr[], int start, int mid, int end) {
-    // int temp[] = new int[end - start + 1];
-    // int i = start;
-    // int j = mid + 1;
-    // int k = 0;
-    // while (i <= mid && j <= end) {
-    // if (arr[i] < arr[j]) {
-    // temp[k] = arr[i];
-    // i++;
-    // } else {
-    // temp[k] = arr[j];
-    // j++;
-    // }
-    // k++;
-    // }
+    }
 
-    // while (i <= mid) {
-    // temp[k++] = arr[i++];
-    // }
+    public static void merging(int arr[], int start, int mid, int end) {
+        int temp[] = new int[end - start + 1];
+        int i = start;
+        int j = mid + 1;
+        int k = 0;
+        while (i <= mid && j <= end) {
+            if (arr[i] < arr[j]) {
+                temp[k] = arr[i];
+                i++;
+            } else {
+                temp[k] = arr[j];
+            }
+            k++;
+        }
 
-    // while (j <= end) {
-    // temp[k++] = arr[j++];
-    // }
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
 
-    // for (k = 0, i = start; k < temp.length; k++, i++) {
-    // arr[i] = temp[k];
-    // }
-    // }
+        while (j <= end) {
+            temp[k++] = arr[j++];
+        }
+
+        for (k = 0, i = start; k < temp.length; k++, i++) {
+            arr[i] = temp[k];
+        }
+    }
 
     public static void mergeSort(int arr[], int si, int ei) {
         if (si >= ei) {
@@ -102,10 +101,13 @@ public class MergeSort {
 
     public static void main(String[] args) {
         int arr[] = { 9, 3, 4, 1, 6, 5, 8, 7, 2, -3, -5 };
-        // mergingSort(arr, 0, arr.length - 1);
-        // displayArray(arr);
-
         mergeSort(arr, 0, arr.length - 1);
         displayArray(arr);
+
+        System.out.println();
+
+        mergingSort(arr, 0, arr.length - 1);
+        displayArray(arr);
+
     }
 }
