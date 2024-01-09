c8aa91a50999953d11c213af7708a9374bbd1de2
md Akhlak <md.22scse1010882@galgotiasuniversity.edu.in>
2024-01-09 19:04:52 +0530

    Delete error

--- JAVA-DSA/Main/Main.java
+++
@@ -1,9 +0,0 @@
-package MAIN;
-
-class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("This will create javac class file");
-
-    }
-}
